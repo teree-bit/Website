c6127bf9abd981f87135bc99edab8114848fcb5c
teree-bit <220401062@student.umri.ac.id>
2024-07-23 08:50:51 +0700

    Website

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+//Responsive Navigation
+
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector(".nav");
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active")
+        burger.classList.toggle("toggle-burger");
+    });
+}
+
+// Clear form before unload
+
+window.onbeforeunload = function() {
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset();
+    }
+};
+
